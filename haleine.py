f5d1cce24633891fe7451f77b4116dbf160b968f
Collapskate <74920986+Collapskate@users.noreply.github.com>
2021-05-13 19:43:11 +0300

    Update haleine.py

--- haleine.py
+++ haleine.py
@@ -246,5 +246,5 @@ def get_api():
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    #port = int(os.environ.get('PORT', 5000))
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, host='0.0.0.0')
